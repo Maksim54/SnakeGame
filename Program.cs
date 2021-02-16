dacb6eec77c2246b872ce1c29966e3fc0638787f
Maksim54 <77334211+Maksim54@users.noreply.github.com>
2021-02-16 10:36:34 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace SnakeGame
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
+            /*p1.x=1;
+            p1.y=3;
+            p1.sym= '*';*/
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
